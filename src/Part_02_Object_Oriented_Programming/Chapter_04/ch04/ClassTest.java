1e393d668c684bd98dba3bc6ceb1b3213c6a8d1f
beautiflow <1114ny@naver.com>
2022-11-30 14:31:55 +0900

    20221130

--- src/Part_02_Object_Oriented_Programming/Chapter_04/ch04/ClassTest.java
+++ src/Part_02_Object_Oriented_Programming/Chapter_04/ch04/ClassTest.java
@@ -26,24 +26,5 @@ public class ClassTest {
 
         Person kim2 = new Person("Kim");
 
-
-//    public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-//            ClassNotFoundException, NoSuchMethodException, SecurityException {
-//        Person person = new Person("James");
-//        System.out.println(person);
-//
-//        Class c1 = Class.forName("ch04.Person");
-//        Person person1 = (Person)c1.newInstance();
-     //   Person person1 = (Person)c1.instance();
-//        System.out.println(person1);
-//
-//        Class[] parameterTypes = {String.class};
-//        Constructor cons = c1.getConstructor(parameterTypes);
-//
-//        Object[] initargs = {"김유신"};
-//        Person personLee = (Person)cons.newInstance(initargs);
-//        System.out.println(personLee);
-//
-//    }
     }
 }
--- src/Part_02_Object_Oriented_Programming/Chapter_04/ch04/StringTestf.java
+++ src/Part_02_Object_Oriented_Programming/Chapter_04/ch04/StringTestf.java
@@ -8,12 +8,15 @@ public class StringTestf {
 
         Class c = Class.forName("java.lang.String");
 
+        System.out.println("------------------getConstructors() 메서드------------------");
+
         Constructor[] cons =  c.getConstructors();
         for(Constructor co : cons){
             System.out.println(co);
         }
 
-        System.out.println();
+        System.out.println(" ");
+        System.out.println("------------------getMethods() 메서드------------------");
 
         Method[] m = c.getMethods();
         for(Method mth : m){
