15df72a161c4aeccc59d7c36451918629bf14251
beautiflow <1114ny@naver.com>
2022-12-10 10:23:01 +0900

    20221209

---
+++ src/Part_02/Chapter_08/ch06/maze/Move.java
@@ -0,0 +1,14 @@
+package Part_02.Chapter_08.ch06.maze;
+
+public class Move {
+
+    int direction = 0;
+
+    public int x = 0;
+    public int y = 0;
+
+    public Move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
